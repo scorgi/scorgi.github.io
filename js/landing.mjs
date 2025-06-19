c1198669b94afc481bc0f459276acf4bd0d1794f
Rob Wiscount <robertwiscount@gmail.com>
2025-06-19 12:47:29 -0400

    Formatted Files

--- js/landing.mjs
+++ js/landing.mjs
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {   
-    // Existing intersection observer logic
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("come-in");
-            }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  // Existing intersection observer logic
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("come-in");
+      }
     });
+  });
 
-    document.querySelectorAll(".banner-section .banner-image").forEach((img) => {
-        observer.observe(img);
-    });
+  document.querySelectorAll(".banner-section .banner-image").forEach((img) => {
+    observer.observe(img);
+  });
 
-    // Initialize AOS
-    // @ts-expect-error - AOS is not defined in the global scope
-    AOS.init({
-        duration: 250,
-        easing: "ease-in-out",
-        once: false,
-        mirror: true,
-        offset: 120,
-    });
+  // Initialize AOS
+  // @ts-expect-error - AOS is not defined in the global scope
+  AOS.init({
+    duration: 250,
+    easing: "ease-in-out",
+    once: false,
+    mirror: true,
+    offset: 120,
+  });
 });
